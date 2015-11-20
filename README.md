9178c838d9d185a7fc488265da03c1998e0e0fc5
Allegra Larche <allegralarche@gmail.com>
2015-11-20 15:35:01 -0500

    updated readme with instructions and writes to output file

---
+++ README.md
@@ -0,0 +1,12 @@
+**** USAGE ****
+
+Make sure you do this:
+mysql -u root
+GRANT SELECT ON randomTwitter_by_month.* TO 'username'@'localhost';
+GRANT SELECT ON randomTwitter_by_month.* TO 'username'@'%';
+
+First run in command prompt: 
+ssh username@ssh.wwbp.org -i "path_to_private_key" -L 3306:127.0.0.1:3306:3306 -N
+
+Second run in different command prompt:
+python script.py <username> <path to output file>
--- script.py
+++ script.py
@@ -16,16 +16,7 @@ import sys
 '''
 **** USAGE ****
 
-Make sure you do this:
-mysql -u root
-GRANT SELECT ON randomTwitter_by_month.* TO 'username'@'localhost';
-GRANT SELECT ON randomTwitter_by_month.* TO 'username'@'%';
-
-First run in command prompt: 
-ssh username@ssh.wwbp.org -i "path_to_private_key" -L 3306:127.0.0.1:3306:3306 -N
-
-Second run in different command prompt:
-script.py username
+In README
 
 '''
 
@@ -34,7 +25,7 @@ def main(argv):
     username = ''
     # private_key_file = ''
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         exit()
 
     username = str(sys.argv[1])
@@ -51,9 +42,11 @@ def main(argv):
     f = open(filename, 'w')
     
     # test
-    cursor.execute("SELECT message FROM messages_es_2014_07_country LIMIT 1")
-    for (message) in cursor:
-        print("Message is : {}".format(message))
+    cursor.execute("SELECT message FROM messages_es_2014_07_country LIMIT 2")
+    for message in cursor:
+        #print("Message is : {}".format(message))
+        f.write(str(message))
+        f.write("\n")
 
     #close file and connection
     f.close()
