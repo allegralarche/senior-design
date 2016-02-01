cd1e1bfd99d8ed72197116faa9b28d9c5869065a
kzembroski <kzembroski@gmail.com>
2016-02-01 16:04:10 -0500

    labels and predictions

--- report.py
+++ report.py
@@ -9,19 +9,18 @@ import nltk
 import sys
 from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import accuracy_score
 
 
 '''
 **** USAGE ****
 
-python getTaggedFile.py inputfile.txt output.txt
-
-Note: Startup takes a little while
+python report.py labels.txt predicted.txt
 
 '''
 def main(argv):
     if len(sys.argv) != 3:
-        print("Usage:> python getTaggedFile.py infile.txt outfile.txt")
+        print("Usage:> python getTaggedFile.py labels.txt predicted.txt")
         exit()
 
     infile1_name = str(sys.argv[1])
@@ -29,13 +28,19 @@ def main(argv):
 
     infile1 = open(infile1_name, 'r')
     infile2 = open(infile2_name, 'r')
-    r1 = read(infile1)
-    print(r1)
+    r1 = infile1.read().splitlines()
+    r1 = [int(i) for i in r1]
+    r2 = infile2.read().splitlines()
+    r2 = [int(i) for i in r2]
 
-    '''y_true = 
-    y_predicted = 
-    precision, recall, thresholds = precision_recall_curve(y_true, y_predicion)
-    '''
+    y_true = r1
+    y_pred = r2
+    accuracy = accuracy_score(y_true, y_pred)
+    precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+
+    print("Accuracy: %0.4f" % accuracy)
+    print("Precision: %0.4f" % precision[1])
+    print("Recall: %0.4f" % recall[1])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
