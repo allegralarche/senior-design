6ccc0d16f7caeea74e64b555d6213133c551fe3f
Joe Raso <joeraso24@gmail.com>
2016-03-14 15:28:33 -0400

    Updated

---
+++ webapp/app/appStyles.js
@@ -0,0 +1,5 @@
+module.exports =  {
+	".angular-google-map-container": {
+		height: "400px"
+	}
+}
---
+++ webapp/app/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var CFControllers = angular.module('CFControllers', []);
+
+CFControllers.controller('HomeCtrl', ['$scope',
+	function($scope) {
+
+		$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+		console.log($scope);
+	}]);
+
+module.exports = CFControllers;
+
