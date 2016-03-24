13c1689e87d5f036494a9508ce650b6d29cf9680
kzembroski <kzembroski@gmail.com>
2016-03-24 11:57:32 -0400

    90 percent accuracy 60 percent precision 56 percent recall

--- getCFFromTagged.py
+++ getCFFromTagged.py
@@ -73,23 +73,32 @@ def identify(tagged_line):
 
 def getForm(tagged_line):
 
+
+    # CASE 0 WISH VERB FORM
+    # p0 = re.compile('\.*/will/MD/', re.IGNORECASE)
+    # pq = re.compile('\.*\?.*')
+    # if p0.search(tagged_line) != None or pq.search(tagged_line) != None:
+    #     return 0
+
     # CASE 1 WISH VERB FORM
     p1 = re.compile('\.*(wish|wishing)/(VB.*/|JJ/)', re.IGNORECASE)
     if p1.search(tagged_line) != None:
         return 1
 
-        # CASE 4 MODAL NORMAL
-    p2 = re.compile('\.*/MD/.*((/VBN/)|(/VBD/)).*/MD/.*((/VBN/)|(/VBD/)|(/VB/)|(VBZ))', re.IGNORECASE)
+
+    # CASE 2 CONJUNTION NORMAL
+    p2 = re.compile('\.*/CCJ/.*((/VBD/)|(/VBN/)).*/MD/', re.IGNORECASE)
     if p2.search(tagged_line) != None:
         return 2
 
-    # CASE 2 CONJUNTION NORMAL
-    p3 = re.compile('\.*((/CC/)|(/IN/)).*((/VBD/)|(/VBN/)).*/MD/', re.IGNORECASE)
+
+    # CASE 3 CONJUNCTIVE CONVERSE
+    p3 = re.compile('\.*/MD/.*/CCJ/.*((/VBN/)|(/VBD/))', re.IGNORECASE)
     if p3.search(tagged_line) != None:
         return 3
 
-    # CASE 3 CONJUNCTIVE CONVERSE
-    p4 = re.compile('\.*/MD/.*/IN/.*((/VBN/)|(/VBD/))', re.IGNORECASE)
+    # CASE 4 MODAL NORMAL
+    p4 = re.compile('\.*/MD/.*((/VBN/)|(/VBD/)).*/MD/.*((/VBN/)|(/VBD/)|(/VB/)|(VBZ))', re.IGNORECASE)
     if p4.search(tagged_line) != None:
         return 4
 
@@ -99,10 +108,12 @@ def getForm(tagged_line):
     #    return 4
 
 
-
-    # CASE 5 HYPOTHETICAL NORMAL
+    # CASE  HYPOTHETICAL NORMAL -> now included in CCJ
     # Key words: rather, imagine, envision, conceptualize, conjure up, visualize
-    p5 = re.compile('\.*((rather/)|(imagine/)|(envision/)|(conceptualize/)|(conjure/)|(visualize/)).*/VB.*/.*/MD/', re.IGNORECASE)
+
+
+    # CASE 5 Should have
+    p5 = re.compile('\.*/((should)|(shoulda)(shulda)|(shuda)|(shudda)|(shudve))/MD/((have)|(hve)|(ve))/(\w)*((/VBN/)|(/VBD/))', re.IGNORECASE)
     if p5.search(tagged_line) != None:
         return 5
 
@@ -143,6 +154,8 @@ def main(argv):
 
     y_pred = []
 
+    form_vec = []
+
     print("Reading file...")
     tagged_line = infile.readline()
 
@@ -151,6 +164,8 @@ def main(argv):
         
         # Get Counterfactual form
         form = int(getForm(tagged_line))
+
+        form_vec.append(form)
 
         # Increase counter
         cf_count[form][0] = cf_count[form][0] + 1
@@ -196,11 +211,11 @@ def main(argv):
         report_file.write("Form %d   Count: %d,  Accuracy: %0.4f \n" % (i, cf_count[i][0], c))
 
     report_file.write("Incorrect predictions\n")
-    report_file.write("idx, pred, label\n")
+    report_file.write("idx, pred, label, form\n")
 
     for i in xrange(len(y_true)):
         if(int(y_true[i]) != int(y_pred[i])):
-            report_file.write(str(i + 1) + " " + str(y_pred[i]) +  " " + str(y_true[i]) + "\n")
+            report_file.write(str(i + 1) + "  " + str(y_pred[i]) +  "  " + str(y_true[i]) + "  " + str(form_vec[i]) + "\n")
 
     report_file.close()
 
